6e78d23bd33bbbf8f5e201159f4e40efdde3db19
Alessandro Napoli <alessandro.luigi.napoli@gmail.com>
2026-06-17 20:21:35 +0200

    Add files via upload

---
+++ src/script/script.js
@@ -0,0 +1,7 @@
+const title = document.getElementById('title');
+const colors = ['red', 'green', 'blue', 'orange', 'purple', 'pink', 'yellow'];
+function updateColorForTitle() {
+    setInterval(() => {
+        title.style.color = colors[Math.floor(Math.random() * colors.length)];
+    }, 700);
+}
